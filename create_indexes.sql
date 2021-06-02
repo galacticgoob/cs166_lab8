8cbee21ed877f20f069b3ffed5f33730334ec9ed
james ooi <65988711+galacticgoob@users.noreply.github.com>
2021-06-01 22:05:02 -0700

    Add files via upload

--- create_indexes.sql
+++ create_indexes.sql
@@ -1,1 +1,8 @@
+DROP INDEX IF EXISTS nyc_part_on_hand_idx;
+CREATE INDEX nyc_part_on_hand_idx
+ON part_nyc(on_hand);
+
+DROP INDEX IF EXISTS nyc_part_supplier_idx;
+CREATE INDEX nyc_part_supplier_idx
+ON part_nyc(supplier);
 
